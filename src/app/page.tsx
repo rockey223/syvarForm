97a5b57aa3e6d61a728f4c32ac92a30929820146
rockey223 <rockeym50@gmail.com>
2023-11-16 13:57:48 +0545

    disabled right click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,9 @@ import { BiSolidPhoneCall } from "react-icons/bi";
 import { GrMail } from "react-icons/gr";
 import Link from "next/link";
 export default function Home() {
+  window.addEventListener(`contextmenu`, (e) => {
+    e.preventDefault();
+});
   return (
     <motion.main className={style.homeMain}>
       <div className={style.heading}>
@@ -41,59 +44,56 @@ export default function Home() {
         </motion.div>
 
         <div className={style.contactBox}>
-
-        <motion.div
-          className={style.contactSection}
-          initial={{ y: 200 }}
-          animate={{ y: 0 }}
-          transition={{
-            delay: 1.2,
-            type: "spring",
-            stiffness: 60,
-          }}
-        >
-          <div className={style.contactTitle}>
-            Any Queries? Feel Free to contact us:
-          </div>
-          <div className={style.contactDetails}>
-            <Link href='https://www.facebook.com/syvartech'>
-            <motion.div
-              className={style.contactFacebook}
-              whileHover={{ scale: 1.2, originX: 0 }}
-            >
-              <BsFacebook />
-              <motion.span whileHover={{ color: "#FFB736" }}>
-                https://www.facebook.com/syvartech
-              </motion.span>
-            </motion.div>
-                  </Link>
-                  <Link href='tel:+977-9840-575-736'>
-            <motion.div
-              className={style.contactPhone}
-              whileHover={{ scale: 1.2, originX: 0 }}
-            >
-              <BiSolidPhoneCall />
-              <motion.span whileHover={{ color: "#FFB736" }}>
-              +977-9840-575-736
-              </motion.span>
-            </motion.div>
-            </Link>
-            <Link href='mailto:info@syvar.com.np'>
-            <motion.div
-              className={style.contactMail}
-              whileHover={{ scale: 1.2, originX: 0 }}
-            >
-              <GrMail />
-              <motion.span whileHover={{ color: "#FFB736" }}>
-                info@syvar.com.np
-              </motion.span>
-            </motion.div>
-            </Link>
-          </div>
-        </motion.div>
+          <motion.div
+            className={style.contactSection}
+            initial={{ y: 200 }}
+            animate={{ y: 0 }}
+            transition={{
+              delay: 1.2,
+              type: "spring",
+              stiffness: 60,
+            }}
+          >
+            <div className={style.contactTitle}>
+              Any Queries? Feel Free to contact us:
+            </div>
+            <div className={style.contactDetails}>
+              <Link href="https://www.facebook.com/syvartech">
+                <motion.div
+                  className={style.contactFacebook}
+                  whileHover={{ scale: 1.2, originX: 0 }}
+                >
+                  <BsFacebook />
+                  <motion.span whileHover={{ color: "#FFB736" }}>
+                    https://www.facebook.com/syvartech
+                  </motion.span>
+                </motion.div>
+              </Link>
+              <Link href="tel:+977-9840-575-736">
+                <motion.div
+                  className={style.contactPhone}
+                  whileHover={{ scale: 1.2, originX: 0 }}
+                >
+                  <BiSolidPhoneCall />
+                  <motion.span whileHover={{ color: "#FFB736" }}>
+                    +977-9840-575-736
+                  </motion.span>
+                </motion.div>
+              </Link>
+              <Link href="mailto:info@syvar.com.np">
+                <motion.div
+                  className={style.contactMail}
+                  whileHover={{ scale: 1.2, originX: 0 }}
+                >
+                  <GrMail />
+                  <motion.span whileHover={{ color: "#FFB736" }}>
+                    info@syvar.com.np
+                  </motion.span>
+                </motion.div>
+              </Link>
+            </div>
+          </motion.div>
         </div>
-
-        
 
         <div className={style.footer}>
           &copy; All Right Reserved by Syvar Technology
